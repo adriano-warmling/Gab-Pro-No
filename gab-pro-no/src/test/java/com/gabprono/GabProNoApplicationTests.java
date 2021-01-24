e950d7176dd2254bfbed26abd9129a31f500950b
Adriano Warmling <warmling.adriano@gmail.com>
2021-01-24 14:56:13 -0300

    Commit inicial

---
+++ gab-pro-no/src/test/java/com/gabprono/GabProNoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabprono;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GabProNoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
